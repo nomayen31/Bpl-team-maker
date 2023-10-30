179eeb3c764eb790d53c78b8b1aa35a9101712ef
nomayen <nomayen.ohin@gmail.com>
2023-10-31 04:45:26 +0600

    dynamic data load

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,28 +1,43 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable react/jsx-key */
 import { useEffect, useState } from "react";
 import './Home.css'
 const Home = () => {
-     const [allActors, setallActors] = useState([])
+     const [allActors, setallActors] = useState([]); 
     useEffect(() =>{
             fetch('../../../public/data.json')
             .then(res => res.json())
             .then(data => setallActors(data))
     },[]) 
+
+    console.log(allActors);
+
     console.log(allActors);
     return (
         <div className='container'>
-            <div className="card-container">
-                <div className="card">
-                   <div className="card-img">
-                    <img className='photo' src="https://i.ibb.co/Rvc67Cr/ar.jpg" alt="" />
-                   </div>
-                   <h2>Lorem, ipsum.</h2>
-                   <p><small>Lorem ipsum dolor sit amet.</small></p>
-                   <div className='info'>
-                    <p>Salary: 200</p></div>
-                    <p>Writer: </p>
-                    <div>
-                        <button className='card-btn'>Select</button>
-                    </div>
+            <div className="home-container">
+               <div className="card-container">
+                {
+                    
+                    allActors.map((actor) =>(
+                        <div key={actor.id} className="card">
+                        <div className="card-img">
+                         <img className='photo' src={actor.image} alt="" />
+                        </div>
+                        <h2>{actor.name}</h2>
+                        <p><small>Lorem ipsum dolor sit amet.</small></p>
+                        <div className='info'>
+                         <p>Salary: {actor.salary}$</p></div>
+                         <p>Writer: {actor.role}</p>
+                         <div>
+                             <button className='card-btn'>Select</button>
+                         </div>
+                     </div>
+                    ))
+                }
+               </div>
+                <div className="cart">
+                    <h1>This is Cart</h1>
                 </div>
             </div>
         </div>
