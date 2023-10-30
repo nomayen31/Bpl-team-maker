9df0c013bfd0abb286da584f08cf97fb6160ca19
nomayen <nomayen.ohin@gmail.com>
2023-10-30 21:10:22 +0600

    data load on site

--- src/Components/Cart/Cart.jsx
+++ src/Components/Cart/Cart.jsx
@@ -3,7 +3,7 @@ import './Cart.css'
 const Cart = () => {
     return (
         <div>
-            
+            <h1>This is cart</h1>
         </div>
     );
 };
--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,8 +1,30 @@
+import { useEffect, useState } from "react";
 import './Home.css'
 const Home = () => {
+     const [allActors, setallActors] = useState([])
+    useEffect(() =>{
+            fetch('../../../public/data.json')
+            .then(res => res.json())
+            .then(data => setallActors(data))
+    },[]) 
+    console.log(allActors);
     return (
         <div className='container'>
-            <h1>This is Home</h1>
+            <div className="card-container">
+                <div className="card">
+                   <div className="card-img">
+                    <img className='photo' src="https://i.ibb.co/Rvc67Cr/ar.jpg" alt="" />
+                   </div>
+                   <h2>Lorem, ipsum.</h2>
+                   <p><small>Lorem ipsum dolor sit amet.</small></p>
+                   <div className='info'>
+                    <p>Salary: 200</p></div>
+                    <p>Writer: </p>
+                    <div>
+                        <button className='card-btn'>Select</button>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
